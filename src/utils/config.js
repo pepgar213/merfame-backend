f979685f2bdf7488747d25073fd0833bbf0baa82
pepgar213 <pepgar213@gmail.com>
2025-10-19 19:08:13 +0200

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,9 +2,14 @@
 const JWT_SECRET = process.env.JWT_SECRET
 const PORT = process.env.PORT;
 const BASE_URL = process.env.BASE_URL; // ✅ Nueva línea
+export const RESEND_API_KEY = process.env.RESEND_API_KEY;
+export const RESEND_FROM = process.env.RESEND_FROM || 'Merfame <onboarding@resend.dev>';
 
 export {
   JWT_SECRET,
   PORT,
-  BASE_URL
+  BASE_URL,
+  RESEND_API_KEY,
+  RESEND_FROM
+
 };
